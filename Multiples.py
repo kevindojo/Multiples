b5b89b3c212adc863bad4ab0186d9a54ec951da5
KevinPham <kevinpham15@gmail.com>
2017-10-02 17:56:01 -0700

    completed

---
+++ Multiples.py
@@ -0,0 +1,22 @@
+## Multiples
+
+# Part I
+
+for odd in range(1, 1001,2):
+    print odd
+
+# Part II
+
+for multiples in range(5, 1000001, 5):
+    print multiples
+
+## Sum List
+
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+## Average List
+
+b = [1, 2, 5, 10, 255, 3]
+x= sum(b)/len(b)
+print x
